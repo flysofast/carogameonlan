94e2b8bec34134221f6c5bf3a7dff8866e89a0a9
tranhuulac1992@gmail.com <tranhuulac1992@gmail.com@985d186e-465c-58f3-3a14-dc1d369e2da0>
2014-01-05 03:11:30 +0000

    Client

---
+++ trunk/Caro_Client/Server/Y2Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+public class Y2Client
+{
+
+    private const int BUFFER_SIZE = 1024;
+    private const int PORT_NUMBER = 9999;
+
+    static ASCIIEncoding encoding = new ASCIIEncoding();
+
+    public static void Main()
+    {
+
+        try
+        {
+            TcpClient client = new TcpClient();
+
+            // 1. connect 169.254.217.101
+            client.Connect("127.0.0.1", PORT_NUMBER);
+            Stream stream = client.GetStream();
+
+            Console.WriteLine("Connected to CaroServer.");
+            Console.Write("Enter your name: ");
+            while (true)
+            {
+                string str = Console.ReadLine();
+                var reader = new StreamReader(stream);
+                var writer = new StreamWriter(stream);
+                writer.AutoFlush = true;
+                // 2. send
+                writer.WriteLine(str);
+                // 3. receive
+                str = reader.ReadLine();
+                Console.WriteLine(str);
+                if (str.ToUpper() == "BYE")
+                    break;
+            }
+            // 4. close
+           // stream.Close();
+           // client.Close();
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex);
+        }
+
+        Console.Read();
+    }
+}
